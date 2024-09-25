6574465f767d9074e6fcec9fd625d68c118af905
Tùng Anh <tuganhhy@gmail.com>
2024-09-26 03:06:14 +0700

    new

---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone --recursive https://github.com/openshwprojects/OpenBK7231N
+git clone --recursive https://github.com/openshwprojects/OpenBK7231T
+git clone https://github.com/openshwprojects/OpenBK7231T_App.git
+cp -r ./OpenBK7231T_App ./OpenBK7231N/apps/OpenBK7231N_App
+cp -r ./OpenBK7231T_App ./OpenBK7231T/apps/OpenBK7231T_App
